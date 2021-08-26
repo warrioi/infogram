7cfdcb664297d5a93077562b7016e9d90e4cf4a1
Cristhian Daniel Rojas <rojasdani248@gmail.com>
2021-08-26 13:17:51 -0400

    toolbar

---
+++ app/src/main/java/com/example/infogram/CreateAcoountActivity.kt
@@ -0,0 +1,24 @@
+package com.example.infogram
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.appcompat.widget.Toolbar
+import kotlinx.android.synthetic.*
+import kotlinx.android.synthetic.main.appbar_toolbar.*
+
+class CreateAcoountActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_create_acoount)
+        showToolBar()
+        }
+    
+    public fun showToolBar(String titulo , boolean botonSubir) {
+        Toolbar toolbar=clearFindViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setTitle(titulo);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(botonSubir);
+    }
+
+}
